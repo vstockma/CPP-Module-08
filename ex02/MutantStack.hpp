8a39637bdf15b6f9483c44c6e5c177d4a934af7a
Valentin Stockmayer <vstockma@c3r1p2.42vienna.com>
2023-11-20 16:30:44 +0100

    ex02 done

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: vstockma <vstockma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 11:30:06 by vstockma          #+#    #+#             */
-/*   Updated: 2023/11/17 14:14:21 by vstockma         ###   ########.fr       */
+/*   Updated: 2023/11/20 12:54:48 by vstockma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,40 @@
 template <typename T>
 class MutantStack : public std::stack<T>
 {
-    private:
-    
     public:
         MutantStack()
         {
+            std::cout << "MutantStack constructor called" << std::endl;
         }
         MutantStack(const MutantStack& copy)
         {
             *this = copy;
+            std::cout << "Copy MutantStack constructor called" << std::endl;
         }
         MutantStack& operator=(const MutantStack& copy)
         {
-            
+            this->c = copy.c;
+            std::cout << "MutantStack Copy assignment operator called" << std::endl;
+            return *this;
         }
         ~MutantStack()
         {
+            std::cout << "MutantStack destructor called" << std::endl;
         }
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        iterator begin()
+        {   return this->c.begin(); }
+
+        iterator end()
+        {   return this->c.end();   }
+
+        const_iterator begin() const
+        {   return this->c.begin(); }
+
+        const_iterator end() const
+        {   return this->c.end();   }
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vstockma <vstockma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 11:34:22 by vstockma          #+#    #+#             */
-/*   Updated: 2023/11/15 11:35:11 by vstockma         ###   ########.fr       */
+/*   Updated: 2023/11/20 16:04:35 by vstockma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 
 int main()
 {
+    //MutantStack
     MutantStack<int> mstack;
 
     mstack.push(5);
     mstack.push(17);
 
+    //This line prints the top element of the stack
     std::cout << mstack.top() << std::endl;
 
+    //This line removes the top element from the stack.
     mstack.pop();
 
+    //Print the size of the stack:
     std::cout << mstack.size() << std::endl;
 
     mstack.push(3);
@@ -38,9 +42,53 @@ int main()
     --it;
     while (it != ite)
     {
-    std::cout << *it << std::endl;
-    ++it;
+        std::cout << *it << std::endl;
+        ++it;
     }
     std::stack<int> s(mstack);
-    return 0;
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    // Test const functions
+    const MutantStack<int> constMutantStack(mstack);
+
+    // Iterate over the elements using const_iterator
+    std::cout << "Const MutantStack elements using const_iterator: ";
+    for (MutantStack<int>::const_iterator it = constMutantStack.begin(); it != constMutantStack.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    //Same main but with std::list
+    // std::list<int> myList;
+
+    // myList.push_back(5);
+    // myList.push_back(17);
+
+    // std::cout << myList.back() << std::endl;
+
+    // myList.pop_back();
+
+    // std::cout << myList.size() << std::endl;
+
+    // myList.push_back(3);
+    // myList.push_back(5);
+    // myList.push_back(737);
+    // //[...]
+    // myList.push_back(0);
+
+    // std::list<int>::iterator it_list = myList.begin();
+    // std::list<int>::iterator ite_list = myList.end();
+
+    // ++it_list;
+    // --it_list;
+    // while (it_list != ite_list) {
+    //     std::cout << *it_list << std::endl;
+    //     ++it_list;
+    // }
+
+    // std::stack<int, std::list<int> > s_list(myList);
+    
 }
